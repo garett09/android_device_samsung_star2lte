fb2f3051c51ff78039b5fc68446a449472740bc7
Adrian Sian <adriansian@gmail.com>
2021-07-12 10:34:17 +0800

    starlte: source update

--- aosp_star2lte.mk
+++ aosp_star2lte.mk
@@ -23,10 +23,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, device/samsung/star2lte/device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common AOSP stuff.
+# Inherit some common Lineage stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_INCLUDE_LIVE_WALLPAPERS := false
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := star2lte
