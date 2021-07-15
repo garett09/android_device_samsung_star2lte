620360a8827e4c562eef6166afa6861b42910570
Adrian Sian <adriansian@gmail.com>
2021-07-15 14:24:08 +0800

    star2lte: july fp

--- lineage_star2lte.mk
+++ lineage_star2lte.mk
@@ -35,7 +35,7 @@ PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
-BUILD_FINGERPRINT := samsung/star2ltexx/star2lte:10/QP1A.190711.020/G965FXXUCFTK1:user/release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=star2ltexx \
