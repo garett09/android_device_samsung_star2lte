7dec864d11d00156ccb27e7e44978d877550c57f
Adrian Sian <adriansian@gmail.com>
2021-11-16 14:38:56 +0800

    star2lte: it's SNOW time

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_star2lte.mk
-
-COMMON_LUNCH_CHOICES := \
-     aosp_star2lte-userdebug \
-     aosp_star2lte-eng \
-     aosp_star2lte-user
+    $(LOCAL_DIR)/evolution_star2lte.mk
